117bf8df2c863ff9b13d921e8ea702b9e667e2d7
Mathias Kiær <saihtame@gmail.com>
2025-05-14 00:03:44 +0200

    Added base classes

---
+++ DataAcces/SQLConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class SQLConnection : IDataAccess
+    {
+        const String ConnectionString = "Data Source=uclprojects.database.windows.net;Integrated Security=false;User ID=serverlogin;Password=1234Login;\r\n\r\n";
+
+        public SQLConnection()
+        {
+
+        }
+    }
+}
--- SemesterProjektRealBoligWinforms/Login.cs
+++ SemesterProjektRealBoligWinforms/Login.cs
@@ -19,15 +19,20 @@ namespace SemesterProjektRealBoligWinforms
 
         private void godkendAdminKnap_Click(object sender, EventArgs e)
         {
+            String username = adminBrugernavnTekstbox.Text;
+            String password = adminPasswordTekstbox.Text;
+
+
+
             // Tjekker om brugernavnet og kodeordet er korrekt
             // Her skal brugernavn og password kontrolleres med databasen evt skal der her være en liste med oprettede brugere 
-           
+
             // For nuværende er det hardkodet til "admin" og "admin"
             // I en rigtig applikation skal du bruge en database til at gemme og kontrollere brugernavne og adgangskoder
             // Dette er kun til demonstration
-            
+
             // indsæt database kode her eller metode til at hente brugernavn og password fra en database
-            
+
             if (adminBrugernavnTekstbox.Text == "admin" && adminPasswordTekstbox.Text == "admin")
             {
                 // Opretter en instans af AdminForm og viser den
