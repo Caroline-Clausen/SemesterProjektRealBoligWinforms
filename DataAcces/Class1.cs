54a2e5fdc5c3cb7b2522c5e9d059430177dddacb
Caroline <caroline@groth-clausen.dk>
2025-05-14 07:44:17 +0200

    små ændringer

--- DataAcces/Class1.cs
+++ DataAcces/Class1.cs
@@ -3,6 +3,9 @@ namespace DataAccess
 {
     public class Class1
     {
-
+        // lav forbindelse til database og opretter hvis den ikke findes
+       
+        
+        // lav metode til at hente data fra database
     }
 }
