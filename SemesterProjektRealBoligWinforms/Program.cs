bc8a8a76ac264b8c34b7f41ddce299fec6f2da5b
Mathias Kiær <saihtame@gmail.com>
2025-05-14 05:03:09 +0200

    Removed dead code

--- SemesterProjektRealBoligWinforms/Program.cs
+++ SemesterProjektRealBoligWinforms/Program.cs
@@ -14,25 +14,8 @@ namespace SemesterProjektRealBoligWinforms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-
-            List<Bolig> boliger = new List<Bolig>();
-            List<Ejendomsmaegler> ejendomsmaeglere = new List<Ejendomsmaegler>();
-            List<Administrator> administratorer = new List<Administrator>();
-            List<Person> personer = new List<Person>();
-            List<Saelger> saelgere = new List<Saelger>();
-            List<Koeber> koebere = new List<Koeber>();
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             // Det her skal pege på den form du vil starte med:
             Application.Run(new Form1());
-
         }
-
-
     }
-
-
-
 }
