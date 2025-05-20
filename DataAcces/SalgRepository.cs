949495e71c6694f5249264c307ccb39bfce339af
Mathias Kiær <saihtame@gmail.com>
2025-05-21 00:06:07 +0200

    Added "HentSalg"

--- DataAcces/SalgRepository.cs
+++ DataAcces/SalgRepository.cs
@@ -10,6 +10,38 @@ namespace DataAccess
 {
     public class SalgRepository : RepositoryBase
     {
+        public Salg? HentSalg(int boligId)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM salg wher boligID = @boligID", con)
+                command.Parameters.AddWithValue("@boligID", boligId.ToString());
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader == null)
+                    {
+                        return null;
+                    }
+                    while (reader.Read())
+                    {
+                        Salg salg = new Salg
+                        {
+                            SalgID = Convert.ToInt32(reader["salgsID"]), 
+                            SaelgerID = Convert.ToInt32(reader["sælgerID"]),
+                            EjendomsmaeglerID = Convert.ToInt32(reader["ejendomsmæglerID"]),
+                            KoeberID = Convert.ToInt32(reader["køberID"]),
+                            Salgsdato = Convert.ToDateTime(reader["salgstidspunkt"]),
+                            Salgspris = Convert.ToInt32(reader["pris"])
+                        };
+                        return salg;
+                    }
+                }
+            }
+
+            return null;
+        }
+
         public void OpretSalg(Salg salg)
         {
             using (SqlConnection con = new SqlConnection(ConnString))
