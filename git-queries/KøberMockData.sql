2f665e349b3fbbbdd0b669b5559ff01d313a282e
Caroline-Clausen <caroline@groth-clausen.dk>
2025-05-20 18:47:34 +0200

    Create KøberMockData.sql
    køber mock data

---
+++ git-queries/KøberMockData.sql
@@ -0,0 +1,7 @@
+INSERT INTO køber (navn, telefonnummer, mail)
+VALUES
+    ('Mathias Kiær', 12345678, 'mathias.kiaer@example.com'),
+    ('Lars Jensen', 23456789, 'lars.jensen@example.com'),
+    ('Camilla Hansen', 34567890, 'camilla.hansen@example.com'),
+    ('Peter Nielsen', 45678901, 'peter.nielsen@example.com'),
+    ('Sofie Pedersen', 56789012, 'sofie.pedersen@example.com');
