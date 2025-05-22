4bbbfef6f54edb1f44a3df9463c2d8e428f81ba5
Mathias Kiær <saihtame@gmail.com>
2025-05-22 02:44:47 +0200

    Removed dead code

--- Model.RealBolig/Ejendomsmaegler.cs
+++ Model.RealBolig/Ejendomsmaegler.cs
@@ -9,31 +9,7 @@ namespace Projekt1Semester
     public class Ejendomsmaegler : Person
     {
         public int EjendomsmaeglerID { get; set; } //Primarnøgle tildelt fra database (autogenereret)
-        public string EjendomsmaeglerBrugernavn { get; set; } //Brugernavn
-        public string EjendomsmaeglerPassword { get; set; } //Password
-
-
-        // arver fra Person
-
-        //En metode hvor Administrator opretter en ny Ejendomsmaegler
-        public Ejendomsmaegler()
-        {
-            EjendomsmaeglerID = 0;
-        }
-        // en metode hvor Administrator får mulighed for at ændre password på Ejendomsmaegler
-        public void ÆndrePassword(string nyPassword)
-        {
-            EjendomsmaeglerPassword = nyPassword;
-        }
-        // en metode hvor Administrator får mulighed for at ændre brugernavn på Ejendomsmaegler
-        public void ÆndreBrugernavn(string nyBrugernavn)
-        {
-            EjendomsmaeglerBrugernavn = nyBrugernavn;
-        }
-        // en metode hvor Administrator får mulighed for at ændre navn på Ejendomsmaegler
-        public void ÆndreNavn(string nyNavn)
-        {
-            PersonNavn = nyNavn;
-        }
+        public string? EjendomsmaeglerBrugernavn { get; set; } //Brugernavn
+        public string? EjendomsmaeglerPassword { get; set; } //Password
     }
 }
