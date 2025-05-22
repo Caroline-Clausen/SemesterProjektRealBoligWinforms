678fc2304b9d7c2c2dbbb37643d350cd92f0c34f
Mathias Kiær <saihtame@gmail.com>
2025-05-22 05:40:44 +0200

    Missing semicolon

--- SemesterProjektRealBoligWinforms/MæglerForms/FilterInfoForm.cs
+++ SemesterProjektRealBoligWinforms/MæglerForms/FilterInfoForm.cs
@@ -40,7 +40,7 @@ namespace SemesterProjektRealBoligWinforms
                 SortValues.ShoppingDistanceMin = GetIntFromTextBox(ShoppingDistanceMinBox);
             if (ShoppingDistanceMaxBox.Text != "")
                 SortValues.ShoppingDistanceMax = GetIntFromTextBox(ShoppingDistanceMaxBox);
-            String? statusStr = StatusComboBox.GetItemText(StatusComboBox.SelectedItem)
+            String? statusStr = StatusComboBox.GetItemText(StatusComboBox.SelectedItem);
             if (statusStr != null && statusStr != "")
             {
                 SortValues.Status = statusStr;
