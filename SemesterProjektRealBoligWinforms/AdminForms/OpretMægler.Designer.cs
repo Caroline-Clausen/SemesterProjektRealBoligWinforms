578707dbbf673cca02379c324ef595fc5dd57c9c
Caroline <caroline@groth-clausen.dk>
2025-05-22 13:48:52 +0200

    halvvejs

--- SemesterProjektRealBoligWinforms/AdminForms/OpretMægler.Designer.cs
+++ SemesterProjektRealBoligWinforms/AdminForms/OpretMægler.Designer.cs
@@ -31,10 +31,6 @@
             components = new System.ComponentModel.Container();
             ejendomsmaeglerBindingSource = new BindingSource(components);
             vistMæglerDataGridView = new DataGridView();
-            ejendomsmaeglerIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            personNavnDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            personEmailDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            personTelefonDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
             opretMæglerKnap = new Button();
             fjernMæglerLable = new Label();
             fjernMæglerKnap = new Button();
@@ -44,6 +40,12 @@
             listeMæglerLable = new Label();
             opdaterMæglerListeKnap = new Button();
             opretMæglerLable = new Label();
+            ejendomsmaeglerIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            EjendomsmaeglerBrugernavn = new DataGridViewTextBoxColumn();
+            EjendomsmaeglerPassword = new DataGridViewTextBoxColumn();
+            Navn = new DataGridViewTextBoxColumn();
+            Email = new DataGridViewTextBoxColumn();
+            Telefon = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)ejendomsmaeglerBindingSource).BeginInit();
             ((System.ComponentModel.ISupportInitialize)vistMæglerDataGridView).BeginInit();
             SuspendLayout();
@@ -58,50 +60,26 @@
             vistMæglerDataGridView.AllowUserToDeleteRows = false;
             vistMæglerDataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             vistMæglerDataGridView.AutoGenerateColumns = false;
+            vistMæglerDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
             vistMæglerDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            vistMæglerDataGridView.Columns.AddRange(new DataGridViewColumn[] { ejendomsmaeglerIDDataGridViewTextBoxColumn, personNavnDataGridViewTextBoxColumn, personEmailDataGridViewTextBoxColumn, personTelefonDataGridViewTextBoxColumn });
+            vistMæglerDataGridView.Columns.AddRange(new DataGridViewColumn[] { ejendomsmaeglerIDDataGridViewTextBoxColumn, EjendomsmaeglerBrugernavn, EjendomsmaeglerPassword, Navn, Email, Telefon });
             vistMæglerDataGridView.DataSource = ejendomsmaeglerBindingSource;
-            vistMæglerDataGridView.Location = new Point(316, 46);
+            vistMæglerDataGridView.Location = new Point(594, 93);
+            vistMæglerDataGridView.Margin = new Padding(6);
             vistMæglerDataGridView.MultiSelect = false;
             vistMæglerDataGridView.Name = "vistMæglerDataGridView";
             vistMæglerDataGridView.ReadOnly = true;
+            vistMæglerDataGridView.RowHeadersWidth = 50;
             vistMæglerDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            vistMæglerDataGridView.Size = new Size(454, 342);
+            vistMæglerDataGridView.Size = new Size(843, 730);
             vistMæglerDataGridView.TabIndex = 7;
-            // 
-            // ejendomsmaeglerIDDataGridViewTextBoxColumn
-            // 
-            ejendomsmaeglerIDDataGridViewTextBoxColumn.DataPropertyName = "EjendomsmaeglerID";
-            ejendomsmaeglerIDDataGridViewTextBoxColumn.HeaderText = "EjendomsmaeglerID";
-            ejendomsmaeglerIDDataGridViewTextBoxColumn.Name = "ejendomsmaeglerIDDataGridViewTextBoxColumn";
-            ejendomsmaeglerIDDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // personNavnDataGridViewTextBoxColumn
-            // 
-            personNavnDataGridViewTextBoxColumn.DataPropertyName = "PersonNavn";
-            personNavnDataGridViewTextBoxColumn.HeaderText = "PersonNavn";
-            personNavnDataGridViewTextBoxColumn.Name = "personNavnDataGridViewTextBoxColumn";
-            personNavnDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // personEmailDataGridViewTextBoxColumn
-            // 
-            personEmailDataGridViewTextBoxColumn.DataPropertyName = "PersonEmail";
-            personEmailDataGridViewTextBoxColumn.HeaderText = "PersonEmail";
-            personEmailDataGridViewTextBoxColumn.Name = "personEmailDataGridViewTextBoxColumn";
-            personEmailDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // personTelefonDataGridViewTextBoxColumn
-            // 
-            personTelefonDataGridViewTextBoxColumn.DataPropertyName = "PersonTelefon";
-            personTelefonDataGridViewTextBoxColumn.HeaderText = "PersonTelefon";
-            personTelefonDataGridViewTextBoxColumn.Name = "personTelefonDataGridViewTextBoxColumn";
-            personTelefonDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // opretMæglerKnap
             // 
-            opretMæglerKnap.Location = new Point(155, 71);
+            opretMæglerKnap.Location = new Point(288, 151);
+            opretMæglerKnap.Margin = new Padding(6);
             opretMæglerKnap.Name = "opretMæglerKnap";
-            opretMæglerKnap.Size = new Size(155, 23);
+            opretMæglerKnap.Size = new Size(288, 49);
             opretMæglerKnap.TabIndex = 8;
             opretMæglerKnap.Text = "Opret ny ejendomsmægler";
             opretMæglerKnap.UseVisualStyleBackColor = true;
@@ -110,17 +88,19 @@
             // fjernMæglerLable
             // 
             fjernMæglerLable.AutoSize = true;
-            fjernMæglerLable.Location = new Point(20, 367);
+            fjernMæglerLable.Location = new Point(37, 783);
+            fjernMæglerLable.Margin = new Padding(6, 0, 6, 0);
             fjernMæglerLable.Name = "fjernMæglerLable";
-            fjernMæglerLable.Size = new Size(129, 15);
+            fjernMæglerLable.Size = new Size(261, 32);
             fjernMæglerLable.TabIndex = 9;
             fjernMæglerLable.Text = "Fjern ejendomsmægler";
             // 
             // fjernMæglerKnap
             // 
-            fjernMæglerKnap.Location = new Point(155, 363);
+            fjernMæglerKnap.Location = new Point(288, 774);
+            fjernMæglerKnap.Margin = new Padding(6);
             fjernMæglerKnap.Name = "fjernMæglerKnap";
-            fjernMæglerKnap.Size = new Size(155, 23);
+            fjernMæglerKnap.Size = new Size(288, 49);
             fjernMæglerKnap.TabIndex = 10;
             fjernMæglerKnap.Text = "Fjern ejendomsmægler";
             fjernMæglerKnap.UseVisualStyleBackColor = true;
@@ -129,17 +109,19 @@
             // redigerMæglerLable
             // 
             redigerMæglerLable.AutoSize = true;
-            redigerMæglerLable.Location = new Point(37, 221);
+            redigerMæglerLable.Location = new Point(69, 471);
+            redigerMæglerLable.Margin = new Padding(6, 0, 6, 0);
             redigerMæglerLable.Name = "redigerMæglerLable";
-            redigerMæglerLable.Size = new Size(112, 15);
+            redigerMæglerLable.Size = new Size(227, 32);
             redigerMæglerLable.TabIndex = 15;
             redigerMæglerLable.Text = "Rediger oplysninger";
             // 
             // redigerMæglerKnap
             // 
-            redigerMæglerKnap.Location = new Point(155, 217);
+            redigerMæglerKnap.Location = new Point(288, 463);
+            redigerMæglerKnap.Margin = new Padding(6);
             redigerMæglerKnap.Name = "redigerMæglerKnap";
-            redigerMæglerKnap.Size = new Size(155, 23);
+            redigerMæglerKnap.Size = new Size(288, 49);
             redigerMæglerKnap.TabIndex = 16;
             redigerMæglerKnap.Text = "Rediger oplysninger";
             redigerMæglerKnap.UseVisualStyleBackColor = true;
@@ -148,9 +130,10 @@
             // afslutOpretMæglerKnap
             // 
             afslutOpretMæglerKnap.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            afslutOpretMæglerKnap.Location = new Point(663, 406);
+            afslutOpretMæglerKnap.Location = new Point(1231, 866);
+            afslutOpretMæglerKnap.Margin = new Padding(6);
             afslutOpretMæglerKnap.Name = "afslutOpretMæglerKnap";
-            afslutOpretMæglerKnap.Size = new Size(107, 29);
+            afslutOpretMæglerKnap.Size = new Size(199, 62);
             afslutOpretMæglerKnap.TabIndex = 17;
             afslutOpretMæglerKnap.Text = "Afslut";
             afslutOpretMæglerKnap.UseVisualStyleBackColor = true;
@@ -159,18 +142,20 @@
             // listeMæglerLable
             // 
             listeMæglerLable.AutoSize = true;
-            listeMæglerLable.Location = new Point(457, 19);
+            listeMæglerLable.Location = new Point(849, 41);
+            listeMæglerLable.Margin = new Padding(6, 0, 6, 0);
             listeMæglerLable.Name = "listeMæglerLable";
-            listeMæglerLable.Size = new Size(193, 15);
+            listeMæglerLable.Size = new Size(393, 32);
             listeMæglerLable.TabIndex = 18;
             listeMæglerLable.Text = "Liste over aktive ejendomsmæglere";
             // 
             // opdaterMæglerListeKnap
             // 
             opdaterMæglerListeKnap.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            opdaterMæglerListeKnap.Location = new Point(316, 406);
+            opdaterMæglerListeKnap.Location = new Point(587, 866);
+            opdaterMæglerListeKnap.Margin = new Padding(6);
             opdaterMæglerListeKnap.Name = "opdaterMæglerListeKnap";
-            opdaterMæglerListeKnap.Size = new Size(107, 29);
+            opdaterMæglerListeKnap.Size = new Size(199, 62);
             opdaterMæglerListeKnap.TabIndex = 19;
             opdaterMæglerListeKnap.Text = "Opdater liste";
             opdaterMæglerListeKnap.UseVisualStyleBackColor = true;
@@ -179,17 +164,72 @@
             // opretMæglerLable
             // 
             opretMæglerLable.AutoSize = true;
-            opretMæglerLable.Location = new Point(16, 75);
+            opretMæglerLable.Location = new Point(30, 160);
+            opretMæglerLable.Margin = new Padding(6, 0, 6, 0);
             opretMæglerLable.Name = "opretMæglerLable";
-            opretMæglerLable.Size = new Size(133, 15);
+            opretMæglerLable.Size = new Size(269, 32);
             opretMæglerLable.TabIndex = 20;
             opretMæglerLable.Text = "Opret ejendomsmægler";
             // 
+            // ejendomsmaeglerIDDataGridViewTextBoxColumn
+            // 
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.DataPropertyName = "EjendomsmaeglerID";
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.HeaderText = "Mægler ID";
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.MinimumWidth = 10;
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.Name = "ejendomsmaeglerIDDataGridViewTextBoxColumn";
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.ReadOnly = true;
+            ejendomsmaeglerIDDataGridViewTextBoxColumn.Width = 172;
+            // 
+            // EjendomsmaeglerBrugernavn
+            // 
+            EjendomsmaeglerBrugernavn.DataPropertyName = "EjendomsmaeglerBrugernavn";
+            EjendomsmaeglerBrugernavn.HeaderText = "Brugernavn";
+            EjendomsmaeglerBrugernavn.MinimumWidth = 10;
+            EjendomsmaeglerBrugernavn.Name = "EjendomsmaeglerBrugernavn";
+            EjendomsmaeglerBrugernavn.ReadOnly = true;
+            EjendomsmaeglerBrugernavn.Width = 182;
+            // 
+            // EjendomsmaeglerPassword
+            // 
+            EjendomsmaeglerPassword.DataPropertyName = "EjendomsmaeglerPassword";
+            EjendomsmaeglerPassword.HeaderText = "Password";
+            EjendomsmaeglerPassword.MinimumWidth = 10;
+            EjendomsmaeglerPassword.Name = "EjendomsmaeglerPassword";
+            EjendomsmaeglerPassword.ReadOnly = true;
+            EjendomsmaeglerPassword.Width = 156;
+            // 
+            // Navn
+            // 
+            Navn.DataPropertyName = "Navn";
+            Navn.HeaderText = "Navn";
+            Navn.MinimumWidth = 10;
+            Navn.Name = "Navn";
+            Navn.ReadOnly = true;
+            Navn.Width = 115;
+            // 
+            // Email
+            // 
+            Email.DataPropertyName = "Email";
+            Email.HeaderText = "Email";
+            Email.MinimumWidth = 10;
+            Email.Name = "Email";
+            Email.ReadOnly = true;
+            Email.Width = 116;
+            // 
+            // Telefon
+            // 
+            Telefon.DataPropertyName = "Telefon";
+            Telefon.HeaderText = "Telefon";
+            Telefon.MinimumWidth = 10;
+            Telefon.Name = "Telefon";
+            Telefon.ReadOnly = true;
+            Telefon.Width = 138;
+            // 
             // OpretMægler
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 447);
+            ClientSize = new Size(1452, 954);
             Controls.Add(opretMæglerLable);
             Controls.Add(opdaterMæglerListeKnap);
             Controls.Add(listeMæglerLable);
@@ -200,7 +240,8 @@
             Controls.Add(fjernMæglerLable);
             Controls.Add(opretMæglerKnap);
             Controls.Add(vistMæglerDataGridView);
-            MinimumSize = new Size(792, 486);
+            Margin = new Padding(6);
+            MinimumSize = new Size(1449, 956);
             Name = "OpretMægler";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Opret mægler";
@@ -214,7 +255,6 @@
         #endregion
         private BindingSource ejendomsmaeglerBindingSource;
         private DataGridView vistMæglerDataGridView;
-        private DataGridViewTextBoxColumn ejendomsmaeglerIDDataGridViewTextBoxColumn;
         private DataGridViewTextBoxColumn personNavnDataGridViewTextBoxColumn;
         private DataGridViewTextBoxColumn personEmailDataGridViewTextBoxColumn;
         private DataGridViewTextBoxColumn personTelefonDataGridViewTextBoxColumn;
@@ -227,5 +267,11 @@
         private Label listeMæglerLable;
         private Button opdaterMæglerListeKnap;
         private Label opretMæglerLable;
+        private DataGridViewTextBoxColumn ejendomsmaeglerIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn EjendomsmaeglerBrugernavn;
+        private DataGridViewTextBoxColumn EjendomsmaeglerPassword;
+        private DataGridViewTextBoxColumn Navn;
+        private DataGridViewTextBoxColumn Email;
+        private DataGridViewTextBoxColumn Telefon;
     }
 }
